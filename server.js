6ea9b8ed16b702af2d204ab17441d20129c30853
Benjamin Bryant <bryantbc98@gmail.com>
2023-05-08 13:55:38 -0400

    added port constant

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 
+const PORT = process.env.port || 3001;
+
 const app = express();
 
 app.use(express.json());
@@ -16,4 +18,8 @@ app.get("/notes", (req, res) => {
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
