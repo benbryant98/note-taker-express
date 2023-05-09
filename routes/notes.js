275930c505ae3cb9086e68120b5639167d4e9873
Benjamin Bryant <bryantbc98@gmail.com>
2023-05-08 21:26:55 -0400

    added route for deleting note

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,48 @@
-const note = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const note = require("express").Router();
+const {
+  readFromFile,
+  readAndAppend,
+  readFromFile,
+  writeToFile,
+} = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
 
-note.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+note.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-note.post('/', (req, res) => {
-    const { title, text } = req.body;
+note.post("/", (req, res) => {
+  const { title, text } = req.body;
 
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            note_id: uuid(),
-        };
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
 
-        readAndAppend(newNote, './db/db.json');
+    readAndAppend(newNote, "./db/db.json");
 
-        const response = {
-            status: 'succes',
-            body: newNote,
-        };
+    const response = {
+      status: "success",
+      body: newNote,
+    };
 
-        res.json(response);
-    } else {
-        res.json('Error in posting feedback.');
-    }
+    res.json(response);
+  } else {
+    res.json("Error in posting feedback.");
+  }
+});
+
+note.delete("/:id", (req, res) => {
+  const { id } = req.body;
+
+  const notesData = JSON.parse(fs.readFromFile("./db/db.json"));
+  const i = notesData.findIndex((note) => note.id === id);
+  const updatedData = notesData.splice(i, 1);
+  writeToFile(updatedData, "./db/db.json");
+
+  res.send("Note deleted successfully!");
 });
 
 module.exports = note;
